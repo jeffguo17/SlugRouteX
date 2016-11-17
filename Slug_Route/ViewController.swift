fd1bcd0d7547d10efc2bd9f81ebd6f4837f787f4
Jeff <jeff@Jeffs-iMac.local>
2016-11-16 17:08:38 -0800

    Implemented network checks

--- Slug_Route/ViewController.swift
+++ Slug_Route/ViewController.swift
@@ -28,14 +28,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     var busList = [Bus]()
     var busMarkerList = [BusMarker]()
     var visualEffectView: UIView!
+    var numBusLabel: UILabel!
+    var timeLabel: UILabel!
     
+    // Map Key Names
     let mapName = [BusType.loop.rawValue,BusType.upperCampus.rawValue,BusType.outOfService.rawValue, BusType.nightOwl.rawValue, BusType.special.rawValue, BusStopType.innerStop.rawValue,BusStopType.outerStop.rawValue]
     
+    //Map Key Images
     let mapImage = [UIImage(busImage: .loop), UIImage(busImage: .uppercampus), UIImage(busImage: .outofservice), UIImage(busImage: .nightowl), UIImage(busImage: .special),UIImage(busStopImage: .orange_stop), UIImage(busStopImage: .blue_stop)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    GMSServices.provideAPIKey("AIzaSyAtebqP7WjxCl7H7TpYwnBhMyIL8mqnK7o")
+        
+        //Google Map Setup 
+        GMSServices.provideAPIKey("AIzaSyAtebqP7WjxCl7H7TpYwnBhMyIL8mqnK7o")
         
         let mapSize = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height-45)
         
@@ -45,6 +51,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         self.view.addSubview(mapView!)
         
+        //Navigation Bar Title
+        let titleString = NSMutableAttributedString(string: "Slug Route", attributes: [NSFontAttributeName: UIFont(name: "Helvetica", size: 25.0)!])
+
+        titleString.addAttribute(NSForegroundColorAttributeName, value: UIColor(red: 1.00, green: 0.63, blue: 0.00, alpha: 1.0), range: .init(location: 0, length: 4))
+        
+        titleString.addAttribute(NSForegroundColorAttributeName, value: UIColor(red: 0.08, green: 0.4, blue: 0.75, alpha: 1.0), range: .init(location: 5, length: 5))
+        
+        let titleLabel = UILabel()
+        titleLabel.attributedText = titleString
+        titleLabel.sizeToFit()
+        
+        self.navigationItem.titleView = titleLabel
+        
+        //Map Key Button
         let infoButton = UIButton(frame: CGRect(x: self.view.frame.size.width-50, y: 125, width: 35, height: 35))
         
         infoButton.layer.cornerRadius = 0.5 * infoButton.bounds.size.width
@@ -57,14 +77,25 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         mapView?.addSubview(infoButton)
         
-        let numBusLabel = UILabel(frame: CGRect(x: 0, y: view.frame.height-45, width: self.view.frame.width, height: 45))
-            
-        numBusLabel.text = "Testing 123 321"
+        //Number of Service Label
+        numBusLabel = InsetLabel(top:0, bottom: 0, left: 10, right: 10, rect: CGRect(x: 0, y: view.frame.height-45, width: self.view.frame.width/2, height: 45))
         
-        numBusLabel.backgroundColor = UIColor.blue
+        numBusLabel.backgroundColor = UIColor(red: 0.08, green: 0.40, blue: 0.75, alpha: 1.0)
+        numBusLabel.adjustsFontSizeToFitWidth = true
+        numBusLabel.textAlignment = .left
         
         self.view.addSubview(numBusLabel)
         
+        //Time Label
+        timeLabel = InsetLabel(top: 0, bottom:0, left: 0, right: 10, rect: CGRect(x: self.view.frame.width/2, y: numBusLabel.frame.origin.y, width: self.view.frame.width/2, height: numBusLabel.frame.height) )
+        
+        timeLabel.backgroundColor = UIColor(red: 0.08, green: 0.40, blue: 0.75, alpha: 1.0)
+        timeLabel.adjustsFontSizeToFitWidth = true
+        timeLabel.textAlignment = .right
+        
+        self.view.addSubview(timeLabel)
+        
+        // Map Key Popup View
         self.popUpView.layer.cornerRadius = 15
         self.popUpView.clipsToBounds = true
         
@@ -72,6 +103,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         visualEffectView.frame = self.view.bounds
         visualEffectView.backgroundColor = UIColor.gray.withAlphaComponent(0.6)
         
+        //Auto fetch data from http server
         Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.fetchBusHttp), userInfo: nil, repeats: true)
         
         DispatchQueue.main.async {
@@ -92,9 +124,17 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         let task = session.dataTask(with: URL! as URL) {
             (data, response, error) in
-            
+
             if error != nil {
-                print(error!)
+                let offlineString = NSMutableAttributedString(string: "No Internet Connection", attributes: [NSFontAttributeName: UIFont(name: "Helvetica", size: 25.0)!])
+                
+                offlineString.addAttribute(NSForegroundColorAttributeName, value: UIColor(red: 0.96, green: 0.26, blue: 0.21, alpha: 1.0), range: .init(location: 0, length: offlineString.length))
+                
+                DispatchQueue.main.async {
+                    self.numBusLabel.attributedText = offlineString
+                    self.timeLabel.textColor = UIColor(red: 0.96, green: 0.26, blue: 0.21, alpha: 1.0)
+                }
+                
                 return
             }
             
@@ -105,9 +145,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 self.setBusList(resultArray: jsonArray)
                 
             } catch let jsonError {
+                print("jsonError")
                 print(jsonError)
             }
-            
             
         }
         
@@ -146,6 +186,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     private func updateBusMarkers() {
+        if busList.count == 0 {
+            for m in busMarkerList {
+                m.marker.map = nil
+            }
+            busMarkerList.removeAll()
+            
+            return
+        }
+        
         for b in busList {
             
             var mBusMarker: BusMarker?
@@ -167,7 +216,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             
             if let mBusMarker = mBusMarker {
                 mBusMarker.marker.position = b.location.coordinate
-                //animate marker
             } else {
                 var busImage: UIImage?
                 
@@ -205,7 +253,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 }
             }
             
-            if removeCurrentMarker {
+            if removeCurrentMarker == true {
                 busMarkerList[m].marker.map = nil
                 indexToRemove.append(m)
             }
@@ -215,6 +263,39 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         for i in indexToRemove {
             busMarkerList.remove(at: i)
         }
+        
+        indexToRemove.removeAll()
+        
+        //numBus Label Setup
+        let numBusString = NSMutableAttributedString(string: "\(busList.count)", attributes: [NSFontAttributeName: UIFont(name: "Helvetica", size: 25.0)!])
+        
+        var stringShuttle = "Shuttles"
+        
+        if busList.count <= 1 {
+            stringShuttle = "Shuttle"
+        }
+        
+        numBusString.append(NSMutableAttributedString(string: " \(stringShuttle) in Service", attributes: [NSFontAttributeName: UIFont(name: "Helvetica", size: 12.0)!]))
+
+        numBusString.addAttributes([NSForegroundColorAttributeName: UIColor.green], range: .init(location: 0, length: 2))
+        
+        numBusLabel.attributedText = numBusString
+        
+        //Current Time Label Setup
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateFormat = "h: mm a"
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .medium
+        dateFormatter.timeZone = TimeZone(identifier: "PST")
+        
+        let currentTime = dateFormatter.string(from: Date())
+        
+        let currentTimeString = NSMutableAttributedString(string: "\(currentTime)", attributes: [NSFontAttributeName: UIFont(name: "Helvetica", size: 25.0)!])
+        
+        currentTimeString.addAttribute(NSForegroundColorAttributeName, value: UIColor(red: 0.30, green: 0.69, blue: 0.31, alpha: 1.0), range: .init(location: 0, length: currentTimeString.length))
+        
+        timeLabel.attributedText = currentTimeString
         
     }
     
@@ -246,6 +327,40 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.mapName.text = mapName[indexPath.row]
         
         return cell
+    }
+    
+    class InsetLabel: UILabel {
+        var topInset = CGFloat(0)
+        var bottomInset = CGFloat(0)
+        var leftInset = CGFloat(0)
+        var rightInset = CGFloat(0)
+        
+        init(top: Float, bottom: Float, left: Float, right: Float, rect: CGRect) {
+            self.topInset = CGFloat(top)
+            self.bottomInset = CGFloat(bottom)
+            self.leftInset = CGFloat(left)
+            self.rightInset = CGFloat(right)
+            super.init(frame: rect)
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+        }
+        
+        override func drawText(in rect: CGRect) {
+            let insets: UIEdgeInsets = UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+            
+            super.drawText(in: UIEdgeInsetsInsetRect(rect, insets))
+        }
+        
+        override public var intrinsicContentSize: CGSize {
+            var intrinsicSuperViewContentSize = super.intrinsicContentSize
+            
+            intrinsicSuperViewContentSize.height += topInset + bottomInset
+            intrinsicSuperViewContentSize.width += leftInset + rightInset
+            
+            return intrinsicSuperViewContentSize
+        }
     }
     
 }
